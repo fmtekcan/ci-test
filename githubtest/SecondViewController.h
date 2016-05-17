186c47f0eb3479f0abbb2bb2500451fd7dbc5f89
fmtekcan <fmtekcan@hotmail.com>
2016-05-17 08:07:26 +0300

    Initial Commit

---
+++ githubtest/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  githubtest
+//
+//  Created by Fatih Tekcan on 17/05/16.
+//  Copyright © 2016 Fatih Tekcan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ githubtest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  githubtest
+//
+//  Created by Fatih Tekcan on 17/05/16.
+//  Copyright © 2016 Fatih Tekcan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
